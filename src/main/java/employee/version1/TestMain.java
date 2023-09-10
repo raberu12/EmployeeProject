12e310af378a3363edd94be534ae4d80cda730da
Matt <11400071@usc.edu.ph>
2023-09-10 17:31:10 +0800

    employee project haha

---
+++ src/main/java/employee/version1/TestMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.version1;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author MAGDADADADADADADADARO, MATT VINCENT
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        
+        LocalDate dateHired = LocalDate.of(2023, 4, 6);
+        LocalDate birthDate = LocalDate.of(2003, 5, 5);
+
+        HourlyEmployee E = new HourlyEmployee(123, "Jacob Dirk", dateHired, birthDate, 40, 10);
+        E.displayInfo();
+    }
+
+}
